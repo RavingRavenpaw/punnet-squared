3e42e6a317bd46ae26b3a0c9a4fac5698ababeaa
John Kozlosky <chromefusion44@gmail.com>
2015-04-13 08:29:10 -0400

    Erasing Phenotype Stuff
    Since this branch will focus on genotype based crossing, which is more important than with phenotypes, it will be more clutter-free and be easier for me to work with.
    
    I don't know if I'l even add in support for using phenotypes.
    I'l definitely do genotypes, maybe phenotypes later when everything's finished. Idk.

--- cat genetics.py
+++ cat genetics.py
@@ -3,7 +3,8 @@ import random
 def cross():
     #L: Short hair
     #l: Long hair
-    #l is dominant to L
+    #L is dominant to l
+    #Short hair is dominant to long hair
     #
     #LL: Short haired cat - Homo. Dom.
     #Ll: Short haired cat - Het. Dom.
@@ -11,33 +12,17 @@ def cross():
     
     
     #1st cat
-    print("Do you know the 1st cat's genotype or phenotype?")
-    print("If phenotype is used, punnet squares with all possible")
-    print("genotypes of the 1st cat will be created.")
-    print("Using genotype will only use the genotype you specify.")
-    print("")
-    typeUsed = raw_input("Type P for Phenotype or G for Genotype: ")
-    if typeUsed == ("G") or typeUsed == ("g"):
-        print("You are using genotype.")
-        print("What is the genotype of the 1st cat?")
-        print("PLEASE PLACE DOMINANT ALLELES BEFORE RECESSIVE ALLELES")
-        print("AND USE CORRECT CAPITALISATION")
-        cat1Genotype = raw_input("Genotype of Cat 1: ")
+    print("What is the genotype of the 1st cat?")
+    print("PLEASE PLACE DOMINANT ALLELES BEFORE RECESSIVE ALLELES")
+    print("AND USE CORRECT CAPITALISATION")
+    cat1Genotype = raw_input("Genotype of Cat 1: ")
 
 
     #2nd cat
-    print("Do you know the 2nd cat's genotype or phenotype?")
-    print("If phenotype is used, punnet squares with all possible")
-    print("genotypes of the 2nd cat will be created.")
-    print("Using genotype will only use the genotype you specify.")
-    print("")
-    typeUsed = raw_input("Type P for Phenotype or G for Genotype: ")
-    if typeUsed == ("G") or typeUsed == ("g"):
-        print("You are using genotype.")
-        print("What is the genotype of the 2nd cat?")
-        print("PLEASE PLACE DOMINANT ALLELES BEFORE RECESSIVE ALLELES")
-        print("AND USE CORRECT CAPITALISATION")
-        cat2Genotype = raw_input("Genotype of Cat 2: ")
+    print("What is the genotype of the 2nd cat?")
+    print("PLEASE PLACE DOMINANT ALLELES BEFORE RECESSIVE ALLELES")
+    print("AND USE CORRECT CAPITALISATION")
+    cat2Genotype = raw_input("Genotype of Cat 2: ")
     #Let's do some math!
     
     genotypesList = [cat1Genotype, cat2Genotype]
