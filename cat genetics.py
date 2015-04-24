51ee158fb250a478413fad1c2d3622901a022186
John Kozlosky <chromefusion44@gmail.com>
2015-04-24 08:38:01 -0400

    Added stuff to do a cross/find gametes

--- cat genetics.py
+++ cat genetics.py
@@ -26,4 +26,35 @@ def cross():
     #Let's do some math!
     
     genotypesList = [cat1Genotype, cat2Genotype]
-    print(genotypesList)
+    #Get alleles
+    
+    #Cat 1
+    cat1Allele1 = (cat1Genotype[:1])
+    print(cat1Allele1)
+    cat1Allele2 = (cat1Genotype[1:2])
+    print(cat1Allele2)
+    
+    #Cat 2
+    cat2Allele1 = (cat2Genotype[:1])
+    print(cat2Allele1)
+    cat2Allele2 = (cat2Genotype[1:2])
+    print(cat2Allele2)
+    
+    
+    '''
+    #Dihybrid cross stuff
+    #I put a bit of stuff in here, but idk if I'll be doing
+    #anything that requires a dihybrid cross yet, so I'll
+    #leave this commented out for now
+    
+    #Get gametes
+    cat1Gamete1 = (cat1Allele1 + cat1Allele2)
+    cat1Gamete2
+    cat1Gamete3
+    cat1Gamete4
+    '''
+    
+    #Now to combine the alleles
+    
+    offspring = [(cat1Allele1 + cat2Allele1), (cat1Allele1 + cat2Allele2), (cat1Allele2 + cat1Allele1), (cat1Allele2 + cat2Allele2)]
+    print(offspring)
