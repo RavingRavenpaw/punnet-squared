ce4d850610826195ac45070130c66199a15079ce
John Kozlosky <chromefusion44@gmail.com>
2015-05-01 20:28:02 -0400

    Taking out the trash...
    Removed junk I don't need, organized what I have, and added some comments.
    Looking pretty good atm.
    
    Eventually I'll just put arguments for the alleles of cats into the function & make sure the input is converted to string(s) so you can just be like "cross(Ll, Ll)" and get shit
    
    Though it's more user-friendly with the current method, and I'm going to show this to people for our 20% project in an interactive demo-like thing so?? Idk
    
    Maybe I'll just make two versions in the end with one like this and the other with arguments for the function
    *shrugs*

--- cat genetics.py
+++ cat genetics.py
@@ -1,6 +1,10 @@
 import random
+#I'll be using this to make more realistic crosses later.
+#Read about it in the wiki. I start talking about stuff like percentages in crosses.
 
 def cross():
+    #BASIC INFO ON GENOTYPES, PHENOTYPES, AMD ALLELES
+    #
     #L: Short hair
     #l: Long hair
     #L is dominant to l
@@ -23,79 +27,33 @@ def cross():
     print("PLEASE PLACE DOMINANT ALLELES BEFORE RECESSIVE ALLELES")
     print("AND USE CORRECT CAPITALISATION")
     cat2Genotype = raw_input("Genotype of Cat 2: ")
-    #Let's do some math!
-    
     genotypesList = [cat1Genotype, cat2Genotype]
+    
+    #Let's do some math!
     #Get alleles
     
     #Cat 1
     cat1Allele1 = (cat1Genotype[:1])
-    print(cat1Allele1)
     cat1Allele2 = (cat1Genotype[1:2])
-    print(cat1Allele2)
     
     #Cat 2
     cat2Allele1 = (cat2Genotype[:1])
-    print(cat2Allele1)
     cat2Allele2 = (cat2Genotype[1:2])
-    print(cat2Allele2)
     
-    
-    '''
-    #Dihybrid cross stuff
-    #I put a bit of stuff in here, but idk if I'll be doing
-    #anything that requires a dihybrid cross yet, so I'll
-    #leave this commented out for now
-    
-    #Get gametes
-    cat1Gamete1 = (cat1Allele1 + cat1Allele2)
-    cat1Gamete2
-    cat1Gamete3
-    cat1Gamete4
-    '''
-    
-    #Now to combine the alleles
-    
+    #Now to combine the alleles as a collective group of offspring
     offspring = [(cat1Allele1 + cat2Allele1), (cat1Allele1 + cat2Allele2), (cat1Allele2 + cat1Allele1), (cat1Allele2 + cat2Allele2)]
-    print(offspring)
     
+    #Combine the alleles into individual offspinrg
     offspring1 = (cat1Allele1 + cat2Allele1)
     offspring2 =  (cat1Allele1 + cat2Allele2)
     offspring3 = (cat1Allele2 + cat1Allele1)
     offspring4 =  (cat1Allele2 + cat2Allele2)
-
-    #From tut, will erase later.
-    #Using as reference to do stuff
-    print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
     
+    #Print a table showing the results of the cross
+    #
+    #It's really basic atm, but it's padded and the data lines up
+    #correctly, so it doesn't matter. I'll make it look pretty
+    #later.
     print "    %s  %s  " % (cat1Allele1, cat1Allele2)
     print "%s  %s  %s  " % (cat2Allele1, offspring1, offspring2)
     print "%s  %s  %s  " % (cat2Allele2, offspring3, offspring4)
-    
-    
-'''
-    #Make a table showing the cross
-    
-    #I was trying to make a table to show the cross (so basically a punnet swaure compsed
-    #of characters
-    #
-    #I'll finish this later or change it into something different, but I'm dropping it for now
-    print(" ",cat1Allele1," "," ",cat1Allele2," ",)
-    print("---------")
-    print("")
-
-        
-
-
-
-    #By Sven Marnach on Stack Overflow
-    #http://stackoverflow.com/questions/8356501/python-format-tabular-output
-    def print_table(table):
-        col_width = [max(len(x) for x in col) for col in zip(*table)]
-        for line in table:
-            print "| " + " | ".join("{:{}}".format(x, col_width[i])
-                                for i, x in enumerate(line)) + " |"
-
-table = (offspring)
-print_table(table)
-'''
