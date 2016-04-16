8ab0fd2f84d36e05423b29d3bbb60100442cb446
Raven <chromefusion44@gmail.com>
2016-04-15 20:37:05 -0400

    Upgraded to Python 3 + VS Studio and Adding Genotype Percents
    SO
    
    I wanted to get this program on Python 3, so I did that. Turns out
    Enthought Canopy doesn't work with anything above Python 2.X, so I
    switched over to Visual Studio and imported my stuff and made a new
    project there, so VS made some files.
    
    Next, I started work on getting the percents and fractions of the unique
    genotypes in the final punnet square

--- cat genetics.py
+++ cat genetics.py
@@ -17,13 +17,13 @@ def cross():
     
     #1st cat
     print("What is the genotype of the 1st cat?")
-    cat1Genotype = raw_input("Genotype of Cat 1: ")
+    cat1Genotype = input("Genotype of Cat 1: ")
     print("")
 
 
     #2nd cat
     print("What is the genotype of the 2nd cat?")
-    cat2Genotype = raw_input("Genotype of Cat 2: ")
+    cat2Genotype = input("Genotype of Cat 2: ")
     print("")
     
     #Let's do some math!
@@ -65,15 +65,56 @@ def cross():
     #Kitten4
     if cat1Allele2 in lowercase_letters and cat2Allele2 in uppercase_letters:
             offspring4 = (cat2Allele2 + cat1Allele2)
+
+    #Get the /unique/ genotypes
+    numGenotypes = 1
+    genotypes = []
+    genotype1 = offspring1
+    genotypes.append(genotype1)
+    if offspring2 != offspring1:
+        genotype2 = offspring2
+        numGenotypes += 1
+        genotypes.append(genotype2)    
+
+    if offspring3 != offspring2 and offspring3 != offspring1:
+        genotype3 = offspring3
+        numGenotypes += 1
+        genotypes.append(genotype3)
+
+    if offspring4 != offspring3 and offspring4 != offspring3 and offspring4 != offspring2 and offspring4 != offspring1:
+        genotype4 = offspring4
+        numGenotypes += 1
+        genotypes.append(genotype4)
+
+    #Convert the list of genotypes to a string
+    #and clean it up
+    genotypesStr = str(genotypes)
+    genotypesStr = genotypesStr.replace(",", "")
+    genotypesStr = genotypesStr.replace("[", "")
+    genotypesStr = genotypesStr.replace("]", "")
+    genotypesStr = genotypesStr.replace("\'", "")
+    
+
+    #Calculate what percent of offpsring are what genotype
+    #[placeholder]
     
     #Print a table showing the results of the cross
     #
     #It's really basic atm, but it's padded and the data lines up
     #correctly, so it doesn't matter. I'll make it look pretty
     #later.
-    print "    %s  %s  " % (cat1Allele1, cat1Allele2)
-    print "%s  %s  %s  " % (cat2Allele1, offspring1, offspring2)
-    print "%s  %s  %s  " % (cat2Allele2, offspring3, offspring4)
-    
+    print("    %s  %s  " % (cat1Allele1, cat1Allele2))
+    print("%s  %s  %s  " % (cat2Allele1, offspring1, offspring2))
+    print("%s  %s  %s  " % (cat2Allele2, offspring3, offspring4))
+    print("")
+    print("Genotypes: %s" % genotypesStr)
+
+def strCleanUp(oldStr):
+    global newStr
+    nsewStr = str(oldStr)
+    newStr = oldStr.replace(",", "")
+    newStr = oldStr.replace("[", "")
+    newStr = oldStr.replace("]", "")
+    newStr = oldStr.replace("\'", "")
 #Run the program once the file is executed
 cross()
